6d0ba2cb863be70b9185a315d2ce797939124639
TheDujin <kevindujin@gmail.com>
2017-02-14 12:59:30 -0800

    Added reader/output

--- src/GoldMoolympics.java
+++ src/GoldMoolympics.java
@@ -1,9 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+//Problem statement at:
+//http://usaco.org/index.php?page=viewproblem2&cpid=381
 
 public class GoldMoolympics {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+  public static void main (String [] args) throws IOException {
+    BufferedReader f = new BufferedReader(new FileReader("recording.in"));
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("recording.out")));
+    StringTokenizer st = new StringTokenizer(f.readLine());
+    int i1 = Integer.parseInt(st.nextToken());
+    int i2 = Integer.parseInt(st.nextToken());
+    out.println(i1+i2);
+    out.close();
+  }
 }
