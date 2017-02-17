b879b10cbd8eb2a6b364e38f9f32d818b88884e6
TheDujin <kevindujin@gmail.com>
2017-02-16 23:09:32 -0800

    Completed wifi but it doesn't work sometimes

--- src/GoldWifiSetup.java
+++ src/GoldWifiSetup.java
@@ -5,15 +5,15 @@ import java.util.*;
 //http://usaco.org/index.php?page=viewproblem2&cpid=381
 
 public class GoldWifiSetup {
-	int cons;
-	int var;
+	double cons;
+	double var;
 	public GoldWifiSetup() throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("wifi.in"));
 	    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("wifi.out")));
 	    StringTokenizer st = new StringTokenizer(f.readLine());
 	    int numCows = Integer.parseInt(st.nextToken());
-	    cons = Integer.parseInt(st.nextToken());
-	    var = Integer.parseInt(st.nextToken());
+	    cons = Double.parseDouble(st.nextToken());
+	    var = Double.parseDouble(st.nextToken());
 	    ArrayList<Router> routers = new ArrayList<Router>();
 	    int[] temp = new int[numCows];
 	    for (int i = 0; i < numCows; i++) {
@@ -23,15 +23,40 @@ public class GoldWifiSetup {
 	    for (int i = 0; i < temp.length; i++) {
 	    	int cow = temp[i];
 	    	int[] cowarray = {cow};
-	    	routers.add(new Router(cow, 0, cowarray));
+	    	routers.add(new Router(0, cowarray));
 		}
 	    boolean madeChange = true;
+	    main:
 	    while (madeChange) {
 	    	madeChange = false;
+	    	for (int i = 0; i < routers.size() - 1; i++) {
+				if (cost(routers.get(i).range) + cost(routers.get(i + 1).range) 
+						> cost((routers.get(i + 1).cowsCovered[routers.get(i + 1).cowsCovered.length - 1]
+						- routers.get(i).cowsCovered[0]) / 2.0)) {
+					temp = routers.get(i).cowsCovered;
+					int[] temp2 = routers.get(i + 1).cowsCovered;
+					int[] newCowsCovered = new int[temp.length + temp2.length];
+					for (int j = 0; j < newCowsCovered.length; j++) {
+						if (j < temp.length)
+							newCowsCovered[j] = temp[j];
+						else
+							newCowsCovered[j] = temp2[j - temp.length];
+					}
+					double newRange = (newCowsCovered[newCowsCovered.length - 1] - newCowsCovered[0]) / 2.0;
+					routers.remove(i + 1);
+					routers.set(i, new Router(newRange, newCowsCovered));
+					madeChange = true;
+					continue main;
+				}
+			}
 	    	
 	    }
+	    double sum = 0;
+	    for (int i = 0; i < routers.size(); i++) {
+			sum += cost(routers.get(i).range);
+		}
 	    
-	    //out.println(i1+i2);
+	    out.println(sum);
 	    out.close();
 	    f.close();
 	}
@@ -39,18 +64,16 @@ public class GoldWifiSetup {
 	public static void main (String [] args) throws IOException {
     new GoldWifiSetup();
   }
-	int cost(int range) {
+	double cost(double range) {
 		return (cons + range * var);
 	}
 
 }
 
 class Router {
-	int center;
-	int range;
+	double range;
 	int[] cowsCovered;
-	Router(int newCenter, int newRange, int[] newCowsCovered) {
-		center = newCenter;
+	Router(double newRange, int[] newCowsCovered) {
 		range = newRange;
 		cowsCovered = newCowsCovered;
 	}
